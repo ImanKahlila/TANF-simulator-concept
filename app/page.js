bbf1e6e5b3fe9ba3ee867f80bfa9c9a697748d88
Iman Kahlila <kayla.iman.bray@gmail.com>
2026-07-29 14:51:16 -0500

    feature: built hero section

--- app/page.js
+++ app/page.js
@@ -1,13 +1,15 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Navbar from "../components/landing/NavBar";
+import HeroSection from "../components/landing/HeroSection";
 
 
 
 export default function Page(){
   return (
-    
-    <Navbar />
-    
+    <>
+      <Navbar />
+      <HeroSection />
+    </>
   )
 }
